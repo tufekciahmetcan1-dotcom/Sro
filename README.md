4bb3dd93a7c9994651dbc0db2ca7e2488947416a
tufekciahmetcan1-dotcom <tufekciahmetcan1@gmail.com>
2026-01-06 14:42:50 +0300

    x
    x

---
+++ README.md
@@ -0,0 +1,1 @@
+# Laravel Standard Template
---
+++ app/Models/User.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable {
+    protected $fillable = ['name','email','password'];
+}
